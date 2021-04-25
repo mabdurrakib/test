31e3d208a25881201d13d2c89d1875018857eb3b
mabdurrakib <mabdurrakib.dev@gmail.com>
2021-04-26 02:52:52 +0600

    first react project

---
+++ my-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+ <div>
+   <h1> To do app </h1>
+   <h3>  call family </h3>
+   <p>
+   </p>
+ </div>
+  document.getElementById('root')
+);
+
